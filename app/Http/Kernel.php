c44816f9c771a95b96df074a72de7415ae250891
ali <alila3883@gmail.com>
2021-06-12 18:55:56 +0300

    remove unneed method in GeneralHelper.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Editor;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,8 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\Admin::class,
-        'editor' => \App\Http\Middleware\Editor::class,
+        'admin' => Admin::class,
+        'editor' => Editor::class,
     ];
 
 }
